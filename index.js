a2a464f53bf913c5e7ed9f2d75920763eed81061
Logan <logansailer0633@gamil.com>
2024-02-07 22:36:35 +0900

    constructor practice

---
+++ index.js
@@ -0,0 +1,14 @@
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+Book.prototype.report = function() {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
+}
+
+let book1 = new Book('The Hobbit', 'J.R.R Tolkein', '294', 'read');
+
+console.log(book1.report());
