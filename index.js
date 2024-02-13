9c5d7526d8dacbac43e9846c445b6b473332eac0
Logan Sailer <logansailer0633@gmail.com>
2024-02-13 16:50:50 +0900

    added DOM manipulation to add books to list

--- index.js
+++ index.js
@@ -1,58 +1,71 @@
 //DOM Elements
 const addButton = document.querySelector("#showDialog");
-const bookDialog = document.querySelector("#newBook");
+const bookDialogue = document.querySelector("#newBook");
 const outputBox = document.querySelector("output");
-const bookTitle = bookDialog.querySelector("#bookTitle");
-const confirmBtn = bookDialog.querySelector("#confirmBtn");
-
-// "Show the dialog" button opens the <dialog> modally
-addButton.addEventListener("click", () => {
-  bookDialog.showModal();
-});
-
-// "Favorite animal" input sets the value of the submit button
-bookTitle.addEventListener("change", (e) => {
-  confirmBtn.value = bookTitle.value;
-});
-
-// "Cancel" button closes the dialog without submitting because of [formmethod="dialog"], triggering a close event.
-bookDialog.addEventListener("close", (e) => {
-  outputBox.value =
-    bookDialog.returnValue === "default"
-      ? "No return value."
-      : `ReturnValue: ${bookDialog.returnValue}.`; // Have to check for "default" rather than empty string
-});
-
-// Prevent the "confirm" button from the default behavior of submitting the form, and close the dialog with the `close()` method, which triggers the "close" event.
-confirmBtn.addEventListener("click", (event) => {
-  event.preventDefault(); // We don't want to submit this fake form
-  bookDialog.close(bookTitle.value); // Have to send the select box value here.
-});
-
+const bookTitle = bookDialogue.querySelector("#bookTitle");
+const bookAuthor = bookDialogue.querySelector("#bookAuthor");
+const bookYear = bookDialogue.querySelector("#bookYear");
+const bookRead = bookDialogue.querySelector("#bookRead");
+const confirmBtn = bookDialogue.querySelector("#confirmBtn");
+const bookList = document.querySelector(".page-layout")
 
 const myLibrary = [];
 
-function Book(title, author, read) {
+function Book(title, author, read, finished) {
     this.title = title;
     this.author = author;
     this.read = read;
+    this.finished = finished;
 }
 
-Book.prototype.report = function() {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
-}
 
-let book1 = new Book('The Hobbit', 'J.R.R Tolkein', '294', 'read');
-
-console.log(book1.report());
+// "Show the dialog" button opens the <dialog> modally
+addButton.addEventListener("click", () => {
+  bookDialogue.showModal();
+});
 
 
+confirmBtn.addEventListener("click", e => {
+    e.preventDefault()
 
-const button = document.querySelector("button");
-button.addEventListener('click', press);
+    const title = bookTitle.value;
+    const author = bookAuthor.value;
+    const year = bookYear.value;
+    const finished = bookRead.value;
+
+    let book = new Book(title, author, year, finished)
+    myLibrary = [...myLibrary, book]
+    displayBook()
+})
 
 
-function press() {
+// "Cancel" button closes the dialog without submitting because of [formmethod="dialog"], triggering a close event.
+bookDialogue.addEventListener("close", (e) => {
+  bookDialogue.close()
+});
+
+
+function displayBook() {
+    let bookDiv = document.createElement("div");
+    bookDiv.classList.add("book");
+
+    let divBookTitle = document.createElement("h3");
+    let divBookAuthor = document.createElement("h4");
+    let divBookYear = document.createElement("p");
+    let divBookFinished = document.createElement("p");
+
+    bookDiv.appendChild(divBookTitle);
+    bookDiv.appendChild(divBookAuthor);
+    bookDiv.appendChild(divBookYear);
+    bookDiv.appendChild(divBookFinished);
+
+    divBookTitle.textContent = 
+    divBookAuthor.textContent = 
+    divBookYEar.textContent = 
+    divBookFinished.textContent = 
+
+    bookList.appendChild(bookDiv)
+    
     let inputText = input.value;
     input.value = "";
     let li = document.createElement("li");
@@ -66,5 +79,9 @@ function press() {
     deleteButton.addEventListener('click', () => {
         list.removeChild(li);
     });
-    input.focus();
 }
+
+
+
+const button = document.querySelector("button");
+button.addEventListener('click', press);
