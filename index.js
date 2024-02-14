d8c39012f32bd7cc1a797de766959d3bb30d0b13
Logan Sailer <logansailer0633@gmail.com>
2024-02-14 09:32:12 +0900

    created functioning delete, changed "finished" to a button

--- index.js
+++ index.js
@@ -27,9 +27,8 @@ confirmBtn.addEventListener("click", e => {
     let title = bookTitle.value;
     let author = bookAuthor.value;
     let year = bookYear.value;
-    let finished = bookRead.value;
+    let finished = bookRead.checked;
     let newBook = new Book(title, author, year, finished)
-    myLibrary += newBook
     displayBook(newBook)
     clearInput()
     bookDialogue.close()
@@ -42,24 +41,44 @@ bookDialogue.addEventListener("close", (e) => {
 
 // creates book object and appends it to the body
 function displayBook(newBook) {
+    //creates DOM elements
     let bookDiv = document.createElement("div");
     bookDiv.classList.add("book");
     let divBookTitle = document.createElement("h2");
     let divBookAuthor = document.createElement("h4");
     let divBookYear = document.createElement("p");
-    let divBookFinished = document.createElement("p");
+    let completedButton = document.createElement("button");
+    let deleteButton = document.createElement("button")
+    //adds class to DOM elements for later styling
     divBookTitle.classList.add("title")
     divBookAuthor.classList.add("author")
     divBookYear.classList.add("year")
-    divBookFinished.classList.add("finished")
+    completedButton.classList.add("finished")
     bookDiv.appendChild(divBookTitle);
     bookDiv.appendChild(divBookAuthor);
     bookDiv.appendChild(divBookYear);
-    bookDiv.appendChild(divBookFinished);
+    bookDiv.appendChild(completedButton);
+    bookDiv.appendChild(deleteButton);
+    //adds text to book info
     divBookTitle.textContent = newBook.title;
     divBookAuthor.textContent = `by ${newBook.author}`;
     divBookYear.textContent = `Written in ${newBook.year}`;
-    divBookFinished.textContent = newBook.finished;
+    //buttons for delete and if read
+    if (newBook.finished === true) {
+        completedButton.textContent = "Completed";
+    } else {
+        completedButton.textContent = "Not Completed";
+    }
+    completedButton.addEventListener('Click', () => {
+        newBook.finished = !newBook.finished
+    })
+
+    deleteButton.textContent = "Delete"
+    deleteButton.addEventListener('click', () => {
+        bookList.removeChild(bookDiv);
+    });
+
+    // adds all of the above to the main Div element
     bookList.appendChild(bookDiv)
 }
 
@@ -68,5 +87,5 @@ function clearInput() {
     bookTitle.value = "";
     bookAuthor.value = "";
     bookYear.value = "";
-    //switchToggle.checked = false;
+    bookRead.checked = false;
 }
